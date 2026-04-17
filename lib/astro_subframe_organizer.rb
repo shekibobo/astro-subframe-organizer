76cab4ec387d3ff2783d977726584bd3a1f2f5c1
Josh Kovach <kovach.jc@gmail.com>
2026-04-17 16:47:39 -0400

    Add FileMetadata value object for parsed file data
    - Create immutable FileMetadata class to hold parsed file attributes
    - Add from_parsed_data() factory method for easy creation from parsers
    - Move computed properties (flatset_id, month, maybe_flat_dark?) to metadata
    - Add 5 new tests for FileMetadata behavior
    - All 35 tests passing (30 original + 5 new)
    - Metadata frozen after creation to prevent mutations

--- lib/astro_subframe_organizer.rb
+++ lib/astro_subframe_organizer.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
-require_relative "astro_subframe_organizer/version"
+require_relative 'astro_subframe_organizer/file_metadata'
 require_relative "astro_subframe_organizer/astrophoto"
-require_relative "astro_subframe_organizer/equipment/telescope"
-require_relative "astro_subframe_organizer/equipment/filter"
 require_relative "astro_subframe_organizer/equipment/camera"
-require_relative "astro_subframe_organizer/path_builders/base_path_builder"
-require_relative "astro_subframe_organizer/path_builders/dark_path_builder"
-require_relative "astro_subframe_organizer/path_builders/flat_path_builder"
-require_relative "astro_subframe_organizer/path_builders/light_path_builder"
-require_relative "astro_subframe_organizer/path_builders/bias_path_builder"
-require_relative "astro_subframe_organizer/path_builder"
-require_relative "astro_subframe_organizer/fits_organizer"
+require_relative "astro_subframe_organizer/equipment/filter"
+require_relative "astro_subframe_organizer/equipment/telescope"
 require_relative "astro_subframe_organizer/filename_parser"
 require_relative "astro_subframe_organizer/filename_parsers/cr2_parser"
 require_relative "astro_subframe_organizer/filename_parsers/fits_parser"
+require_relative "astro_subframe_organizer/fits_organizer"
+require_relative "astro_subframe_organizer/path_builder"
+require_relative "astro_subframe_organizer/path_builders/base_path_builder"
+require_relative "astro_subframe_organizer/path_builders/bias_path_builder"
+require_relative "astro_subframe_organizer/path_builders/dark_path_builder"
+require_relative "astro_subframe_organizer/path_builders/flat_path_builder"
+require_relative "astro_subframe_organizer/path_builders/light_path_builder"
+require_relative "astro_subframe_organizer/version"
 
 require 'fileutils'
 require 'date'
---
+++ lib/astro_subframe_organizer/file_metadata.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+module AstroSubframeOrganizer
+  # Value object representing parsed metadata from an astrophotography file
+  # Immutable after creation to prevent accidental mutations
+  class FileMetadata
+    attr_reader :type, :target, :exposure, :bin, :camera, :gain, :iso, :created_at,
+                :ccd_temp, :image_index, :path, :filename, :telescope, :filter,
+                :dark_flat, :mosaic_pane, :file_format
+
+    def initialize(
+      type:, path:, filename:, file_format:, exposure: nil, bin: nil, camera: nil,
+      iso: nil, gain: nil, created_at: nil, ccd_temp: nil, image_index: nil,
+      target: nil, telescope: nil, filter: nil, dark_flat: false, mosaic_pane: nil
+    )
+      @type = type
+      @path = path
+      @filename = filename
+      @file_format = file_format
+      @exposure = exposure
+      @bin = bin
+      @camera = camera
+      @iso = iso
+      @gain = gain
+      @created_at = created_at
+      @ccd_temp = ccd_temp
+      @image_index = image_index
+      @target = target
+      @telescope = telescope
+      @filter = filter
+      @dark_flat = dark_flat
+      @mosaic_pane = mosaic_pane
+
+      freeze
+    end
+
+    # Factory method to create FileMetadata from parsed parser result
+    def self.from_parsed_data(parsed_data)
+      new(
+        type: parsed_data[:type],
+        path: parsed_data[:path],
+        filename: parsed_data[:filename],
+        file_format: parsed_data[:file_format],
+        target: parsed_data[:target],
+        mosaic_pane: parsed_data[:mosaic_pane],
+        exposure: parsed_data[:exposure],
+        bin: parsed_data[:bin],
+        camera: parsed_data[:camera],
+        iso: parsed_data[:iso],
+        gain: parsed_data[:gain],
+        created_at: parsed_data[:created_at],
+        ccd_temp: parsed_data[:ccd_temp],
+        image_index: parsed_data[:image_index],
+        telescope: parsed_data[:telescope],
+        filter: parsed_data[:filter],
+        dark_flat: parsed_data[:dark_flat] || false
+      )
+    end
+
+    def dark_flat?
+      dark_flat
+    end
+
+    # True if the dark is likely a dark flat and hasn't already been organized as dark flat
+    def maybe_flat_dark?
+      exp_val = exposure.to_f
+      exp_units = exposure.gsub(exp_val.to_s, '')
+      exp_in_seconds = case exp_units
+                       when 's'
+                         exp_val
+                       when 'ms'
+                         exp_val / 1000.0
+                       when 'us'
+                         exp_val / 1_000_000.0
+                       end
+      type == 'Dark' && exp_in_seconds <= 10.0 && !dark_flat?
+    end
+
+    # The date formatted like '20220508'. If pictures taken in latter half of day,
+    # assume flatset will be generated the next day
+    def flatset_id
+      if type == 'Light' && created_at.hour >= 12
+        created_at.next_day.strftime('%Y%m%d')
+      else
+        created_at.strftime('%Y%m%d')
+      end
+    end
+
+    # The Year-Month in which image was taken. Useful for grouping darks by season
+    def month
+      created_at.strftime('%Y-%m')
+    end
+
+    # Current directory of the file
+    def current_dir
+      segments = File.split(path) - [filename]
+      File.join(*segments)
+    end
+
+    # True if the path is already at target destination
+    def already_moved?(target_path)
+      path == target_path
+    end
+  end
+end
