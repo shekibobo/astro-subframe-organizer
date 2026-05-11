77e82295e06174583bcc9602e3342b6397e5ea20
Joshua Kovach <kovach.jc@gmail.com>
2026-05-10 20:30:16 -0400

    Add protections from overwriting config on init, add --force option

--- lib/astro_subframe_organizer/commands/init.rb
+++ lib/astro_subframe_organizer/commands/init.rb
@@ -4,37 +4,51 @@ require 'dry/cli'
 
 module AstroSubframeOrganizer
   module Commands
+    # Create default config file at ~/.astro-subframe-organizer.yml
     class Init < Dry::CLI::Command
       include SharedOptions
 
       desc 'Create default config file at ~/.astro-subframe-organizer.yml'
 
-      def call(**options)
+      option :force, type: :boolean, default: false, required: false, desc: 'Overwrite existing file'
+
+      def call(force: false, **options)
         setup(**options.slice(:config, :verbose, :skip_confirm))
+
         config_file = options[:config] || File.join(ENV['HOME'], '.astro-subframe-organizer.yml')
 
-        default_config = {
-          'telescopes' => %w[
-            RedCat51
-            ZhumellZ130
-            AperturaAD8
-            MeadeDS90
-            CanonEFS1855
-          ],
-          'filters' => %w[
-            BaaderMoon
-            NBZ
-            NoFilter
-          ],
-          'cameras' => %w[
-            T7
-            183MC
-          ],
-          'temperature_tolerance' => 5.0,
-        }
-        require 'yaml'
-        File.write(config_file, default_config.to_yaml)
-        puts 'Created default config file at ~/.astro-subframe-organizer.yml'
+        if File.exist?(config_file) && !force
+          puts "Config file #{config_file} already exists. Use --force to overwrite anyway."
+        else
+          default_config = {
+            'telescopes' => %w[
+              RedCat51
+              ZhumellZ130
+              AperturaAD8
+              MeadeDS90
+              CanonEFS1855
+            ],
+            'filters' => %w[
+              BaaderMoon
+              NBZ
+              NoFilter
+            ],
+            'cameras' => %w[
+              T7
+              183MC
+            ],
+            'temperature_tolerance' => 5.0,
+          }
+          require 'yaml'
+          File.write(config_file, default_config.to_yaml)
+
+          if options[:config]
+            puts "Created config file at #{config_file}"
+          else
+            puts 'Created default config file at ~/.astro-subframe-organizer.yml'
+          end
+        end
+
         puts 'Edit this file to customize your telescopes, filters, and cameras.'
       end
     end
--- spec/astro_subframe_organizer/commands/init_spec.rb
+++ spec/astro_subframe_organizer/commands/init_spec.rb
@@ -7,74 +7,198 @@ describe 'astro-subframe-organizer init', type: :aruba do
   let(:config_path) { File.join(aruba.config.home_directory, '.astro-subframe-organizer.yml') }
   let(:custom_config_path) { File.join(aruba.config.home_directory, 'custom.yml') }
 
+  let(:config) { YAML.load_file(config_path) }
+  let(:custom_config) { YAML.load_file(custom_config_path) }
+
+  let(:simple_config) { { 'temperature_tolerance' => '5.0' } }
+
   context 'with no config option' do
-    before { run_command_and_stop 'astro-subframe-organizer init' }
+    let(:command) { 'astro-subframe-organizer init' }
 
-    it 'exits successfully' do
-      expect(last_command_started.exit_status).to eq(0)
-    end
+    context 'no config file exists yet' do
+      before { run_command_and_stop command }
 
-    it 'creates the config file in the home directory' do
-      expect(File).to exist(config_path)
-    end
-
-    it 'outputs a confirmation message' do
-      expect(last_command_started.output).to include('Created default config file')
-    end
-
-    it 'outputs an instruction to edit the file' do
-      expect(last_command_started.output).to include('Edit this file to customize')
-    end
-
-    describe 'the created config file' do
-      subject(:config) { YAML.load_file(config_path) }
-
-      it 'includes telescopes' do
-        expect(config['telescopes']).to include('RedCat51', 'ZhumellZ130')
+      it 'exits successfully' do
+        expect(last_command_started.exit_status).to eq(0)
       end
 
-      it 'includes filters' do
-        expect(config['filters']).to include('BaaderMoon', 'NBZ', 'NoFilter')
+      it 'creates the config file in the home directory' do
+        expect(File).to exist(config_path)
       end
 
-      it 'includes cameras' do
-        expect(config['cameras']).to include('T7', '183MC')
+      it 'outputs a confirmation message' do
+        expect(last_command_started.output).to include('Created default config file')
+      end
+
+      it 'outputs an instruction to edit the file' do
+        expect(last_command_started.output).to include('Edit this file to customize')
+      end
+
+      describe 'the created config file' do
+        it 'includes telescopes' do
+          expect(config['telescopes']).to include('RedCat51', 'ZhumellZ130')
+        end
+
+        it 'includes filters' do
+          expect(config['filters']).to include('BaaderMoon', 'NBZ', 'NoFilter')
+        end
+
+        it 'includes cameras' do
+          expect(config['cameras']).to include('T7', '183MC')
+        end
+      end
+    end
+
+    context 'when file already exists' do
+      before do
+        File.write(config_path, simple_config.to_yaml)
+        run_command_and_stop command
+      end
+
+      it 'exits successfully' do
+        expect(last_command_started.exit_status).to eq(0)
+      end
+
+      it 'does not overwrite the existing file' do
+        expect(File).to exist(config_path)
+        expect(config).not_to include('cameras', 'telescopes', 'filters')
+      end
+
+      it 'outputs a confirmation message' do
+        expect(last_command_started.output).to include("Config file #{config_path} already exists. Use --force to overwrite anyway.")
+      end
+
+      it 'outputs an instruction to edit the file' do
+        expect(last_command_started.output).to include('Edit this file to customize')
+      end
+
+      context 'with --force option' do
+        let(:command) { 'astro-subframe-organizer init --force' }
+
+        it 'exits successfully' do
+          expect(last_command_started.exit_status).to eq(0)
+        end
+
+        it 'creates the config file in the home directory' do
+          expect(File).to exist(config_path)
+        end
+
+        it 'outputs a confirmation message' do
+          expect(last_command_started.output).to include('Created default config file')
+        end
+
+        it 'outputs an instruction to edit the file' do
+          expect(last_command_started.output).to include('Edit this file to customize')
+        end
+
+        describe 'the created config file' do
+          it 'includes telescopes' do
+            expect(config['telescopes']).to include('RedCat51', 'ZhumellZ130')
+          end
+
+          it 'includes filters' do
+            expect(config['filters']).to include('BaaderMoon', 'NBZ', 'NoFilter')
+          end
+
+          it 'includes cameras' do
+            expect(config['cameras']).to include('T7', '183MC')
+          end
+        end
       end
     end
   end
 
   context 'with --config option' do
-    before { run_command_and_stop "astro-subframe-organizer init --config #{custom_config_path}" }
+    let(:command) { "astro-subframe-organizer init --config #{custom_config_path}" }
 
-    it 'exits successfully' do
-      expect(last_command_started.exit_status).to eq(0)
-    end
+    context 'when no config file exists' do
+      before { run_command_and_stop command }
 
-    it 'creates the config file in the home directory' do
-      expect(File).to exist(custom_config_path)
-    end
-
-    it 'outputs a confirmation message' do
-      expect(last_command_started.output).to include('Created default config file')
-    end
-
-    it 'outputs an instruction to edit the file' do
-      expect(last_command_started.output).to include('Edit this file to customize')
-    end
-
-    describe 'the created config file' do
-      subject(:config) { YAML.load_file(custom_config_path) }
-
-      it 'includes telescopes' do
-        expect(config['telescopes']).to include('RedCat51', 'ZhumellZ130')
+      it 'exits successfully' do
+        expect(last_command_started.exit_status).to eq(0)
       end
 
-      it 'includes filters' do
-        expect(config['filters']).to include('BaaderMoon', 'NBZ', 'NoFilter')
+      it 'creates the config file in the home directory' do
+        expect(File).to exist(custom_config_path)
       end
 
-      it 'includes cameras' do
-        expect(config['cameras']).to include('T7', '183MC')
+      it 'outputs a confirmation message' do
+        expect(last_command_started.output).to include('Created config file')
+      end
+
+      it 'outputs an instruction to edit the file' do
+        expect(last_command_started.output).to include('Edit this file to customize')
+      end
+
+      describe 'the created config file' do
+        it 'includes telescopes' do
+          expect(custom_config['telescopes']).to include('RedCat51', 'ZhumellZ130')
+        end
+
+        it 'includes filters' do
+          expect(custom_config['filters']).to include('BaaderMoon', 'NBZ', 'NoFilter')
+        end
+
+        it 'includes cameras' do
+          expect(custom_config['cameras']).to include('T7', '183MC')
+        end
+      end
+    end
+
+    context 'when file already exists' do
+      before do
+        File.write(custom_config_path, simple_config.to_yaml)
+        run_command_and_stop command
+      end
+      it 'exits successfully' do
+        expect(last_command_started.exit_status).to eq(0)
+      end
+
+      it 'does not overwrite the existing file' do
+        expect(File).to exist(custom_config_path)
+        expect(custom_config).not_to include('cameras', 'telescopes', 'filters')
+      end
+
+      it 'outputs a confirmation message' do
+        expect(last_command_started.output).to include("Config file #{custom_config_path} already exists. Use --force to overwrite anyway.")
+      end
+
+      it 'outputs an instruction to edit the file' do
+        expect(last_command_started.output).to include('Edit this file to customize')
+      end
+
+      context 'with --force option' do
+        let(:command) { "astro-subframe-organizer init --config #{custom_config_path} --force" }
+
+        it 'exits successfully' do
+          expect(last_command_started.exit_status).to eq(0)
+        end
+
+        it 'creates the config file in the home directory' do
+          expect(File).to exist(custom_config_path)
+        end
+
+        it 'outputs a confirmation message' do
+          expect(last_command_started.output).to include('Created config file')
+        end
+
+        it 'outputs an instruction to edit the file' do
+          expect(last_command_started.output).to include('Edit this file to customize')
+        end
+
+        describe 'the created config file' do
+          it 'includes telescopes' do
+            expect(custom_config['telescopes']).to include('RedCat51', 'ZhumellZ130')
+          end
+
+          it 'includes filters' do
+            expect(custom_config['filters']).to include('BaaderMoon', 'NBZ', 'NoFilter')
+          end
+
+          it 'includes cameras' do
+            expect(custom_config['cameras']).to include('T7', '183MC')
+          end
+        end
       end
     end
   end
