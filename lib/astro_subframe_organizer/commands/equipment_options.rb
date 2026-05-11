52830fb2265f0e7e19267d4ca56fd0e803302a2d
Joshua Kovach <kovach.jc@gmail.com>
2026-05-10 22:04:44 -0400

    Add documentation for EquipmentOptinos#set_equipment

--- lib/astro_subframe_organizer/commands/equipment_options.rb
+++ lib/astro_subframe_organizer/commands/equipment_options.rb
@@ -11,6 +11,13 @@ module AstroSubframeOrganizer
 
       attr_reader :equipment_selector
 
+      # Set the equipment on an equipment selector during organizing. If a value is not provided
+      # via command line option, the organizer will follow default rules for determing equipment
+      # selection:
+      #   - If available in filename or FITS headers, and it matches equipment in Config, auto-selected.
+      #   - If not determined, will prompt the user to select an option from Config equipment.
+      #
+      # Only use this if the command uses equipment selector. Otherwise, use +options+ directly.
       def set_equipment(telescope: nil, camera: nil, filter: nil)
         @equipment_selector = EquipmentSelector.new.tap do |eq|
           eq.telescope = telescope
