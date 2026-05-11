640034bc3af0933ccbbad58df0a2cc63ad5f2de7
Joshua Kovach <kovach.jc@gmail.com>
2026-05-10 20:30:42 -0400

    Add examples for run command documentation

--- lib/astro_subframe_organizer/commands/run.rb
+++ lib/astro_subframe_organizer/commands/run.rb
@@ -7,7 +7,14 @@ module AstroSubframeOrganizer
     class Run < Dry::CLI::Command
       include SharedOptions
 
-      desc 'Run the subframe organizer'
+      desc 'Run the subframe organizer interactively'
+
+      example [
+        '                             # Basic usage, using default values or values from default config file at ~/.astro-subframe-organizer.yml',
+        '--config ~/.custom-setup.yml # Uses alternative setup with specific equipment',
+        '--verbose                    # Log more details while running',
+        '--skip-confirm               # Skip confirmation step before moving files',
+      ]
 
       def call(**options)
         setup(**options.slice(:config, :verbose, :skip_confirm))
